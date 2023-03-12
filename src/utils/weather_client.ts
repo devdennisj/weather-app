b4c919c64751fa2ab89a4d134adbecd46e181200
devdennisj <dev@dennisj.se>
2023-03-12 19:58:01 +0100

    Moves appid to axios client config

--- src/utils/weather_client.ts
+++ src/utils/weather_client.ts
@@ -2,15 +2,16 @@ import axios from "axios";
 
 const weatherClient = axios.create({
 	baseURL: 'https://pro.openweathermap.org/data/2.5/forecast',
+	params: {
+		appid: import.meta.env.VITE_API_KEY
+	}
 });
-
-const appendAPIKey = (url: string) => `${url}&appid=${import.meta.env.VITE_API_KEY}`
 
 interface HourlyParams {
 	lon: number;
 	lat: number;
 }
 
-export const hourly = ({ lon, lat }: HourlyParams) => appendAPIKey(`/hourly?lat=${lat}&lon=${lon}`);
+export const hourly = ({ lon, lat }: HourlyParams) => `/hourly?lat=${lat}&lon=${lon}`;
 
 export default weatherClient
